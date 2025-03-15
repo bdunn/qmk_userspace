350b84351f8250c3cb4505a3cb4a5dacaffa49ee
NH115641 <blake.dunn@hertz.com>
2025-03-14 19:27:15 -0500

    Remove unnecessary preprocessor directives in keymap.c
    Eliminated unused #ifdef and #include directives referencing OTHER_KEYMAP_C to simplify and clean up the keymap code. This improves code clarity and removes unused functionality.

--- keyboards/splitkb/halcyon/elora/keymaps/bdunn_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/bdunn_hlc/keymap.c
@@ -97,9 +97,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS
     )
 };
-
-
-
-#ifdef OTHER_KEYMAP_C
-#    include OTHER_KEYMAP_C
-#endif // OTHER_KEYMAP_C
