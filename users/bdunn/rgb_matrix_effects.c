80e13ab140e5b820d478b76fa70bdc15f5429fd0
NH115641 <blake.dunn@hertz.com>
2025-03-16 11:56:05 -0500

    Building my default layout and getting hands down setup. Getting rid of all the other shit.

--- users/bdunn/rgb_matrix_effects.c
+++ users/bdunn/rgb_matrix_effects.c
@@ -23,17 +23,53 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
     // Set color immediately on layer change
     switch (layer) {
+        case 0:
+            rgb_matrix_set_color_all(100, 100, 100); // Soft white
+            break;
         case 1:
-            rgb_matrix_set_color_all(255, 165, 0); // Orange
+            rgb_matrix_set_color_all(255, 165, 0);   // Orange
             break;
         case 2:
-            rgb_matrix_set_color_all(0, 255, 0);   // Green
+            rgb_matrix_set_color_all(0, 255, 0);     // Green
             break;
         case 3:
-            rgb_matrix_set_color_all(0, 0, 255);   // Blue
+            rgb_matrix_set_color_all(0, 0, 255);     // Blue
+            break;
+        case 4:
+            rgb_matrix_set_color_all(255, 0, 0);     // Red
+            break;
+        case 5:
+            rgb_matrix_set_color_all(255, 255, 0);   // Yellow
+            break;
+        case 6:
+            rgb_matrix_set_color_all(0, 255, 255);   // Cyan
+            break;
+        case 7:
+            rgb_matrix_set_color_all(255, 0, 255);   // Magenta
+            break;
+        case 8:
+            rgb_matrix_set_color_all(128, 0, 128);   // Purple
+            break;
+        case 9:
+            rgb_matrix_set_color_all(255, 192, 203); // Pink
+            break;
+        case 10:
+            rgb_matrix_set_color_all(0, 128, 128);   // Teal
+            break;
+        case 11:
+            rgb_matrix_set_color_all(128, 128, 0);   // Olive
+            break;
+        case 12:
+            rgb_matrix_set_color_all(192, 192, 192); // Silver
+            break;
+        case 13:
+            rgb_matrix_set_color_all(255, 140, 0);   // Dark Orange
+            break;
+        case 14:
+            rgb_matrix_set_color_all(0, 0, 0);       // Off
             break;
         default:
-            rgb_matrix_set_color_all(100, 100, 100); // Soft white
+            rgb_matrix_set_color_all(100, 100, 100); // Default to Soft white
             break;
     }
 
