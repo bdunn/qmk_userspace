d0252b0c8e456e0ead01f772f43bc3a619618880
Blake Dunn <a.blake.dunn@gmail.com>
2025-03-18 08:38:41 -0500

    Update key mappings for bdunn_hlc keymap.
    Modified several key assignments, adding KC_Q and KC_Z in specific layers and updating other key bindings. These changes aim to improve typing efficiency and functionality.

--- keyboards/splitkb/halcyon/elora/keymaps/bdunn_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/bdunn_hlc/keymap.c
@@ -31,9 +31,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
        KC_TAB,   KC_V,   KC_W,   KC_G,   KC_M,    KC_J,                                     KC_SCLN,   KC_DOT,   KC_QUOT,   KC_EQL,   KC_SLSH, KC_BSLS,
 
-       KC_NO,  LGUI_T(KC_S), LALT_T(KC_N), LCTL_T(KC_T), LSFT_T(KC_H), KC_K,                KC_COMMA, RSFT_T(KC_A), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_C), KC_NO,
+       KC_NO,  LGUI_T(KC_S), LALT_T(KC_N), LCTL_T(KC_T), LSFT_T(KC_H), KC_K,                KC_COMMA, RSFT_T(KC_A), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_C), KC_Q,
 
-       KC_NO,  KC_F,  KC_P,    KC_D,    KC_L,   KC_X,  KC_ESC, KC_NO,                       KC_NO, KC_NO, KC_MINS,   KC_U,   KC_O,   KC_Y,   KC_B,   KC_NO,
+       KC_NO,  KC_F,  KC_P,    KC_D,    KC_L,   KC_X,  KC_ESC, KC_Q,                       KC_Z, KC_DEL, KC_MINS,   KC_U,   KC_O,   KC_Y,   KC_B,   KC_Z,
 
                                KC_LALT, MO(1),  KC_ENT,  KC_R, KC_SPC,                     KC_ENT,  KC_BSPC, KC_SPC, MO(1), KC_DEL,
 
