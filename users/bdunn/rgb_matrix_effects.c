9da6d65e58d13cbef889f08951877fe12e1109db
Blake Dunn <a.blake.dunn@gmail.com>
2025-06-22 10:57:08 -0500

    Update RGB effects and modify keybinding in keymap
    Adjusted RGB matrix effects to correct color definitions for cases 9 and 10, ensuring accurate visual output. Updated a keybinding to use a layered shortcut (LCTL(LGUI(KC_SPC))) for improved functionality on the Halcyon keyboard.

--- users/bdunn/rgb_matrix_effects.c
+++ users/bdunn/rgb_matrix_effects.c
@@ -44,14 +44,14 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             last_v = 255;
             break;
         case 9:
-            last_h = 255;    // BLUE
+            last_h = 255;    // Orange
             last_s = 191;
             last_v = 0;
             break;
         case 10:
-            last_h = 255;    // Orange
-            last_s = 191;
-            last_v = 0;
+            last_h = 302;    // Purple
+            last_s = 100;
+            last_v = 48;
             break;
         default:
             last_h = 0;      // WHITE (no hue, no saturation, max value)
