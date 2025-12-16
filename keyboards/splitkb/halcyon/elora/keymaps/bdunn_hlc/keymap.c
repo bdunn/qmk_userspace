21888d73d0270852878d504067be1e2f13b173ae
Blake Dunn <a.blake.dunn@gmail.com>
2025-12-16 15:19:07 -0600

    Update hands down layers: add top/bottom flip and refine key assignments
    Revised hands down layers for improved functionality, including the addition of top/bottom flip. Adjusted key assignments for better logical placement and layer behavior consistency.

--- keyboards/splitkb/halcyon/elora/keymaps/bdunn_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/bdunn_hlc/keymap.c
@@ -53,21 +53,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
    ),
 
-// Layer 2: Hands down normal
+// Layer 0: Hands down top/bottom flip
    [2] = LAYOUT_elora_hlc(
        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                                   KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,   KC_BSPC,
-       KC_TAB,   KC_F,  KC_P,    KC_D,    KC_L,   KC_X,  KC_ESC, KC_Q,                                     KC_SCLN,   KC_DOT,   KC_QUOT,   KC_EQL,   KC_SLSH, KC_BSLS,
+       KC_TAB,   KC_F,  KC_P,    KC_D,    KC_L,   KC_X,                                     KC_MINS,   KC_U,   KC_O,   KC_Y,   KC_B, KC_BSLS,
        KC_LCTL,  LGUI_T(KC_S), LALT_T(KC_N), LCTL_T(KC_T), LSFT_T(KC_H), KC_K,                KC_COMMA, RSFT_T(KC_A), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_C), KC_Q,
-       KC_LSFT,  KC_V,   KC_W,   KC_G,   KC_M,    KC_J,                       KC_Z, KC_DEL, KC_MINS,   KC_U,   KC_O,   KC_Y,   KC_B,   KC_Z,
-                               KC_LALT, KC_LGUI,  KC_BSPC,  KC_R, MO(3),                     KC_ENT,  KC_NO, KC_SPC, KC_LGUI, KC_DEL,
+       KC_LSFT,  KC_V,   KC_W,   KC_G,   KC_M,    KC_J,  KC_ESC, KC_Q,                       KC_Z, KC_DEL, KC_SCLN,   KC_DOT,   KC_QUOT,   KC_EQL,   KC_SLSH,   KC_Z,
+                               KC_LALT, KC_LGUI,  KC_BSPC,  KC_R, MO(1),                     KC_ENT,  KC_NO, KC_SPC, KC_LGUI, KC_DEL,
        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
    ),
 
- // Layer 1: Hands Down Normal Function Layer
+ // Layer 1: Hands Down Flip Function Layer
    [3] = LAYOUT_elora_hlc(
        KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                                   KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,   KC_F12,
               KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                                KC_LCBR,   KC_RCBR,   KC_MINS,   KC_EQL,   KC_HOME, KC_PGUP,
-              KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                          KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT,  KC_END, KC_PGDN,
+              TO(1),  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                          KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT,  KC_END, KC_PGDN,
               TO(5),  KC_NO,  KC_NO,    KC_NO,    KC_NO,   KC_NO,  KC_ESC, KC_TRNS,                 TO(9), KC_NO, KC_LBRC,   KC_RBRC,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
                                       KC_TRNS, KC_TRNS,  KC_TRNS,  KC_NO, KC_TRNS,                KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
               KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                                          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
